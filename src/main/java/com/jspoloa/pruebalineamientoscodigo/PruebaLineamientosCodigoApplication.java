bba21d982e9d83c0c8729f46489aae4806841152
José Polo <jpolo@asesoftware.com>
2020-09-02 11:13:40 -0500

    Inicialización del repositorio

---
+++ src/main/java/com/jspoloa/pruebalineamientoscodigo/PruebaLineamientosCodigoApplication.java
@@ -0,0 +1,13 @@
+package com.jspoloa.pruebalineamientoscodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLineamientosCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaLineamientosCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jspoloa/pruebalineamientoscodigo/PruebaLineamientosCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspoloa.pruebalineamientoscodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaLineamientosCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
