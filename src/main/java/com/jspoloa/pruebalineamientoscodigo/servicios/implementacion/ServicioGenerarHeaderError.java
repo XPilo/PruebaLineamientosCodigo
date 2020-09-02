3a798b8555a4eab8056597544d032f6056504f7b
José Polo <jpolo@asesoftware.com>
2020-09-02 11:52:33 -0500

    Se añade el archivo propiedades.properties

--- src/main/java/com/jspoloa/pruebalineamientoscodigo/servicios/implementacion/ServicioGenerarHeaderError.java
+++ src/main/java/com/jspoloa/pruebalineamientoscodigo/servicios/implementacion/ServicioGenerarHeaderError.java
@@ -1,5 +1,7 @@
 package com.jspoloa.pruebalineamientoscodigo.servicios.implementacion;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,16 @@ import com.jspoloa.pruebalineamientoscodigo.enumeradores.CabecerasPorCodigoHttpE
 import com.jspoloa.pruebalineamientoscodigo.servicios.ServicioGenerarHeaderErrorI;
 
 @Service
+@PropertySource("classpath:propiedades.properties")
 public class ServicioGenerarHeaderError implements ServicioGenerarHeaderErrorI{
 
+	@Value("${header.mensaje}")
+	private String headerMensaje;
+	
 	public HttpHeaders generarHeader(HttpStatus codigoError) {
 		HttpHeaders responseHeaders = new HttpHeaders();
 		CabecerasPorCodigoHttpEnum cabecera = CabecerasPorCodigoHttpEnum.buscarPorValor(codigoError);
-		responseHeaders.set("mensaje", cabecera.getMensaje());
+		responseHeaders.set(headerMensaje, cabecera.getMensaje());
 		return responseHeaders;
 	}
 
